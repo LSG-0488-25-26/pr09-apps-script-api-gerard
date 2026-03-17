b61652e0d9b932a0f5fb0ed86b8a1814036d9c91
Gerard Fornés <gerard.fornes@gracia.lasalle.cat>
2026-03-17 20:10:22 +0100

    feat: afegits models Song i Review

---
+++ app/src/main/java/com/example/spotifyapp/model/Review.kt
@@ -0,0 +1,8 @@
+package com.example.spotifyapp.model
+
+data class Review(
+    val track_name: String = "",
+    val artist_name: String = "",
+    val rating: String = "",
+    val comment: String = ""
+)
---
+++ app/src/main/java/com/example/spotifyapp/model/Song.kt
@@ -0,0 +1,9 @@
+package com.example.spotifyapp.model
+
+data class Song(
+    val track_name: String = "",
+    val artist_name: String = "",
+    val streams: String = "",
+    val released_year: String = "",
+    val spotify_popularity: String = ""
+)
